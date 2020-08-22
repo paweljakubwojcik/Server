fce402453cae155802a452ab8797b7cc7f4795e9
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2020-08-22 19:19:21 +0200

    added message when there are no files

--- public/lista.js
+++ public/lista.js
@@ -1,5 +1,5 @@
-let lista = document.querySelector('.main > ul')
-
+let lista = document.querySelector('.main')
+let header = document.querySelector('.main > header')
 let path = document.URL + 'fileTree';
 
 
@@ -9,33 +9,45 @@ fetch(path)
         return response.json()
     })
     .then((data) => {
-        for (const link of data.fileTree) {
-            createListElement(link,lista)
-        }
+        data = data.fileTree;
+        if (data.length != 0)
+            createListElement(data, lista)
+        else
+            createAppologize()
     })
     .catch((err) => {
         // Do something for an error here
     })
 
 
-function createListElement(item,lista)
-{
+function createListElement(item, lista) {
+
     if (!Array.isArray(item)) {
         let li = document.createElement('li')
         li.innerHTML = `<a href='${item}' > ${item} </a>`
         lista.appendChild(li)
     }
-    else
-    {
+    else {
+
+        //tworzenie nagłówka
+        let p = document.createElement('p') 
+        p.innerHTML = item.find((element) => !Array.isArray(element)).split('\\').reverse()[1] || 'MyFiles'; // na serwerze trzeba zamienić '\\' na '/'
+        // znajdź element który jest stringiem -> podziel go na tablice -> odwróć tablice -> weź pierwszy element, jeśli jest pusty zastą 'working tree'
+
         let li = document.createElement('li')
         let ul = document.createElement('ul')
-        let p = document.createElement('p')
-        p.innerHTML = item[0].split('/')[1]
+
         ul.appendChild(p)
         li.appendChild(ul)
         lista.appendChild(li)
         item.forEach(element => {
-            createListElement(element,ul)
+            createListElement(element, ul)
         });
     }
+}
+
+function createAppologize() {
+    let p = header;
+    p.classList.add('apology')
+    p.innerHTML = 'no content here today, sorry ;< '
 }
